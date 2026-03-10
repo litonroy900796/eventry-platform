dd39df736d5512b29a77e1b90bf001e5386eaf8c
litonroy900796 <litonroy900796@gmail.com>
2026-03-10 22:11:57 +0600

    mongodb connect and events list get

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import EventList from "@/components/landing/EventList";
 import Header from "@/components/landing/Header";
+import { getAllEvents } from "@/lib/actions/event.action";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents();
+  console.log(events);
+
   return (
     <section className="container mx-auto">
       <Header />
-      <EventList />
+      <EventList events={events} />
     </section>
   );
 }
--- components/landing/EventList.jsx
+++ components/landing/EventList.jsx
@@ -1,11 +1,13 @@
-import EventCard from "./EventCard"
+import EventCard from "./EventCard";
 
-const EventList = () => {
+const EventList = ({ events }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
-      <EventCard />
+      {events.map((event) => (
+        <EventCard key={event.id} event={event} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default EventList
+export default EventList;
