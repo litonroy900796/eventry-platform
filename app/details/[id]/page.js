7ef0a1566320c1a9cf67364d72fa52d85aeff4aa
litonroy900796 <litonroy900796@gmail.com>
2026-03-10 23:42:18 +0600

    event details page data get

---
+++ app/details/[id]/page.js
@@ -0,0 +1,29 @@
+import EventDetails from "@/components/details/EventDetails";
+import EventVenue from "@/components/details/EventVenue";
+import HeroSection from "@/components/details/HeroSection";
+import { getEventById } from "@/lib/actions/event.action";
+import React from "react";
+
+async function EventDetail({ params }) {
+  const { id } = await params; // ✅ params await করো
+  const eventInfo = await getEventById(id);
+  console.log(eventInfo);
+
+  if (!eventInfo) {
+    return <div>Event not found!</div>;
+  }
+
+  return (
+    <div>
+      <HeroSection event={eventInfo} />
+      <section className="container">
+        <div className="grid grid-cols-5 gap-12 my-12">
+          <EventDetails event={eventInfo} />
+          <EventVenue event={eventInfo} />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default EventDetail;
